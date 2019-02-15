efcf278add6e949f1cbcce5c4e1d29713f94fae3
Wesley Scholl <clients@konjoweb.com>
2019-02-15 15:19:03 -0500

    connected database, tested node, added test get message

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+const express = require("express");
+const parser = require("body-parser");
+const app = express();
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/recipes");
+mongoose.Promise = Promise;
+
+app.use(express.static("public"));
+
+app.set("view engine", "hbs");
+
+app.use(parser.urlencoded({ extended: true }));
+
+const router = express.Router();
+
+router.get("/", function(req, res) {
+  res.render("views/index");
+});
+
+const message = "Recipe!";
+
+app.listen(3008, () => console.log("listening on port 3008"));
+
+app.get("/", (req, res) => {
+  //res.render("layout", { message });
+  res.send("Recipe...");
+});
+
 //database
 
 //routes for crud
